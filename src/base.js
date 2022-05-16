028c094618e41ff2dc371bea0b61db5d57607c60
tuvshinbat <tuvshinbat596@gmail.com>
2022-05-17 02:46:49 +0800

    link changes

--- src/base.js
+++ src/base.js
@@ -1,7 +1,7 @@
 const myBase = {
   cdnUrl: "https://rest.queenbella.mn/",
-  baseUrl: "http://localhost:3000/",
-  siteUrl: "https://rest.queenbella.mn/",
+  baseUrl: "https://admin.queenbella.mn/",
+  siteUrl: "https://beta.queenbella.mn/",
   // cdnUrl: "http://localhost:8022/",
   // baseUrl: "http://localhost:3001/",
   // siteUrl: "http://localhost:3000/",
